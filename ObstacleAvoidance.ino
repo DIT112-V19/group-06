c42785e627e2edfb6e60d17c5134b70e3cc36d92
XiangXu <jiangnan.flyslowly@gmail.com>
2019-05-05 12:18:16 +0200

    Improve obstacle avoidance: keep moving
     The car now can turn right, turn left or turn back when detecting an obstacle to avoid it, and then keep moving on.

--- ObstacleAvoidance.ino
+++ ObstacleAvoidance.ino
@@ -7,12 +7,12 @@ int leftMotorSpeedPin = 9;
 int rightMotorForwardPin = 12;
 int rightMotorBackwardPin = 13;
 int rightMotorSpeedPin = 11;
-const int TRIGGER_PIN = 6; //D6
-const int ECHO_PIN = 7; //D7
+const int TRIGGER_PIN = 52; //D6
+const int ECHO_PIN = 53; //D7
 
 //Initialize magic value
 const unsigned int MAX_DISTANCE = 100;
-const int STOP_DISTANCE = 20;
+const int STOP_DISTANCE = 40;
 
 BrushedMotor leftMotor(leftMotorForwardPin,leftMotorBackwardPin, leftMotorSpeedPin);
 
@@ -28,18 +28,59 @@ void setup() {
  
  Serial.begin(9600);
  
- car.setSpeed(100);
+// car.setSpeed(40);
  
 }
 
 void loop() {
- 
+
  int curDistance = front.getDistance();
 
  if(curDistance < STOP_DISTANCE && curDistance > 0){
-  
-  car.setSpeed(0);
-  
+  if(curDistance < 20 ){
+        Backward();
+        delay(1000);
+        Stop();
+        }
+   while(curDistance < 50 && curDistance > 0){
+        Right();
+        delay(1000);
+        Stop();
+        curDistance = front.getDistance();
+        if(curDistance < 50 && curDistance > 0){
+        Left();
+        delay(1000);
+        Stop();
+        curDistance = front.getDistance();
+        }
+        } 
+  }else{
+    Forward();
   }
   
+}
+
+void Forward(){
+  car.setSpeed(30);
+  car.setAngle(0);
+}
+
+void Backward(){
+  car.setSpeed(-30);
+  car.setAngle(0);
+}
+
+void Left(){
+  car.setSpeed(30);
+  car.setAngle(-110);
+}
+
+void Right(){
+  car.setSpeed(30);
+  car.setAngle(110);
+}
+
+void Stop(){
+  car.setSpeed(0);
+  car.setAngle(0);
 }
